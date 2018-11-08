996ae814e39762639d9e9df9ee8fc05470bb8566
Tae-Yang <tysong94@naver.com>
2018-11-08 18:09:11 +0900

    Node.js-MySQL 코드정리 - 글 수정, 삭제

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 var http = require('http');
 var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var db = require('./lib/db');
 var topic = require('./lib/topic');
 
 var app = http.createServer(function(request,response){
@@ -25,70 +22,12 @@ var app = http.createServer(function(request,response){
     topic.create_process(request, response);
   // 글 UPDATE    
   } else if(pathname === '/update'){
-    db.query(`SELECT * FROM topic`, function(error, topics){
-      if(error) throw error;
-      db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function(error2, topic){
-        if(error2) throw error2;
-        db.query(`SELECT * FROM author`, function(error3, authors){
-          if(error3) throw error3;
-          
-          var list = template.list(topics);
-          var html = template.HTML(
-            topic[0].title, 
-            list,
-            `<a href="/create">create</a>`,
-            `<form action="/update_process" method="post">
-              <p>
-                <input type="hidden" name="id" value="${topic[0].id}">
-              </p>
-              <p>
-                <input type="text" name="title" placeholder="title" value=${topic[0].title}>
-              </p>
-              <p>
-                <textarea name="description" placeholder="description">${topic[0].description}</textarea>
-              </p>
-                ${template.authorSelect(authors, topic[0].author_id)}
-              <p>
-                <input type="submit">
-              </p>
-            </form>`
-          );
-          response.writeHead(200);
-          response.end(html);
-        });
-      });
-    });
+    topic.update(request, response);
   } else if(pathname === '/update_process'){
-    var body = '';
-    request.on('data', function(data){
-        body = body + data;
-    });
-    request.on('end', function(){
-      var post = qs.parse(body);
-      db.query(
-        `UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`,
-        [post.title, post.description, post.author, post.id], 
-        function(error, result){
-          if(error) throw error;
-          response.writeHead(302, {Location: `/?id=${post.id}`});
-          response.end();
-        }
-      );
-    });
+    topic.update_process(request, response);
   // 글 DELETE
   } else if(pathname === '/delete_process'){
-    var body = '';
-    request.on('data', function(data){
-        body = body + data;
-    });
-    request.on('end', function(){
-      var post = qs.parse(body);
-      db.query(`DELETE FROM topic WHERE id=?`, [post.id], function(error, result){
-        if(error) throw error;
-        response.writeHead(302, {Location: `/`});
-        response.end();
-      });
-    });
+    topic.delete_process(request, response);
   // 404 ERROR
   } else {
     response.writeHead(404);
